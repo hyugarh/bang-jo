7ccb698b9e273d166cc3900e5e2d842e1c3bc0e4
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2023-05-16 11:48:17 +0700

    Update

--- componen/page/Account/index.js
+++ componen/page/Account/index.js
@@ -1,71 +1,28 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { Component } from "react";
-import { Text, Touchable, View } from "react-native";
+import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-// export default class index extends Component {
-//   // _logout = async () => {
-//   //   await AsyncStorage.clear();
-//   //   this.props.navigation.navigate("Login");
-//   // };
-
-//   render() {
-//     return (
-//       <View style={{ flex: 1, justifyContent: "center" }}>
-//         <Touchable
-//           style={{
-//             backgroundColor: "lightblue",
-//             height: 40,
-//             width: 100,
-//             alignSelf: "center",
-//             justifyContent: "center",
-//           }}
-//         >
-//           <Text style={{ fontSize: 15, alignSelf: "center" }}>LogOut</Text>
-//         </Touchable>
-//       </View>
-//     );
-//   }
-// }
-
-// Create a component class
 export default class index extends Component {
-  // Setting up Login Activity title.
-  static navigationOptions = {
-    title: "Profile",
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      UserEmail: "",
-    };
-  }
-
-  componentDidMount() {
-    AsyncStorage.getItem("UserEmail").then((value) => {
-      this.setState({ UserEmail: value });
-    });
-  }
-
-  UserLogoutFunction = () => {
-    AsyncStorage.clear();
+  _logout = async () => {
+    await AsyncStorage.clear();
     this.props.navigation.navigate("Login");
   };
 
   render() {
     return (
-      <View>
-        <Text style={{ fontSize: 20, color: "#000", textAlign: "center" }}>
-          {this.state.UserEmail}
-        </Text>
-
-        <TouchableHighlight
-          style={[styles.buttonContainer, styles.loginButton]}
-          onPress={this.UserLogoutFunction}
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <TouchableOpacity
+          onPress={this._logout}
+          style={{
+            backgroundColor: "lightblue",
+            height: 40,
+            width: 100,
+            alignSelf: "center",
+            justifyContent: "center",
+          }}
         >
-          <Text style={styles.loginText}>Logout</Text>
-        </TouchableHighlight>
+          <Text style={{ fontSize: 15, alignSelf: "center" }}>LogOut</Text>
+        </TouchableOpacity>
       </View>
     );
   }
